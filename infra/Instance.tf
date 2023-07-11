3e73100c16b48e77e6ab5311e1971c3e9e1f2de1
André <andreichiro@gmail.com>
2023-07-11 19:55:11 -0300

    Update Instance.tf

--- infra/Instance.tf
+++ infra/Instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "handlers-airflow" {
   ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.medium"
+  instance_type = "t2.small"
   key_name = "testing"
   subnet_id                   = aws_subnet.handlers_vpc_public_subnet.id
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
